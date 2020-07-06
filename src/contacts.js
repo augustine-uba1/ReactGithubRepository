518a168039a1068b30a563a26df98c7370fd26d3
augustine-uba1 <32349243+augustine-uba1@users.noreply.github.com>
2020-07-06 09:10:03 +0100

    Initial commit

---
+++ src/contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    name: "Guardiola",
+    imgURL:
+      "https://i2-prod.dailystar.co.uk/incoming/article22254204.ece/ALTERNATES/s615b/0_Premier-League-Chelsea-v-Manchester-City.jpg",
+    phone: "+123 456 789",
+    email: "b@beyonce.com"
+  },
+  {
+    name: "Idibia",
+    imgURL:
+      "https://ocdn.eu/pulscms-transforms/1/3Ddk9kqTURBXy9hMzhiYjI0ODdhZTY2Yzg0ZjhhZTA5ZjU0NGMzMjhhOS5qcGVnkpUDAA3NAoDNAWiVAs0DBwDDw4KhMAGhMQE",
+    phone: "+987 654 321",
+    email: "jack@nowhere.com"
+  },
+  {
+    name: "Marshall",
+    imgURL:
+      "https://thesource.com/wp-content/uploads/2020/02/shutterstock_1632717139-750x400.jpg",
+    phone: "+918 372 574",
+    email: "gmail@chucknorris.com"
+  }
+];
+
+export default contacts;
